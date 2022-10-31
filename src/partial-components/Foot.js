1deaa614ef6adabfcdc6b53070c7302344625ef9
Gregory Monroe II <gregthemusician@gmail.com>
2022-10-31 00:18:03 -0400

    added boilerplate and export default

--- src/partial-components/Foot.js
+++ src/partial-components/Foot.js
@@ -2,3 +2,21 @@ import React from 'react'
 
 import Switch from '../ui-components/switch/Switch'
 
+import React from 'react'
+
+import Switch from '../ui-components/switch/Switch'
+
+const Foot = () => {
+  return (
+    <>
+      <div className="left">
+        🚀 The Sempiternity Company
+      </div>
+      <div className="right">
+        <Switch />
+      </div>
+    </>
+  )
+}
+
+export default Foot
