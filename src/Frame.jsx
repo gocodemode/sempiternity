b6e9fb8a160f08d122341f5af504989f5bf83933
Gregory Monroe II <gregthemusician@gmail.com>
2022-11-01 00:19:52 -0400

    add partial-components

--- src/Frame.jsx
+++ src/Frame.jsx
@@ -4,3 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 const Home = lazy(() => import('./view-components/Home'))
 const Events = lazy(() => import('./view-components/Events'))
 const LoadingMessage = () => `loading...`
+
+import Logo from './partial-components/Logo'
+import Sidenav from './partial-components/Sidenav'
+import Topnav from './partial-components/Topnav'
+import Foot from './partial-components/Foot'
