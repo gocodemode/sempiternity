cee555904a7d3cd0376785328dde08408a687ab4
Gregory Monroe II <gregthemusician@gmail.com>
2022-11-01 00:15:40 -0400

    home.js

---
+++ src/view-components/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+
+const Home = () => {
+  useEffect(() => {document.title = `Home Page`},[])
+
+  return (
+    <div className='view-home'>
+      <h3>Next Launch</h3>
+      {/* Launch Component */}
+    </div>
+  )
+}
+
+export default Home
