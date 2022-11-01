3478e306eab0e9195f032ab2a8ae5fcd7a77e066
Gregory Monroe II <gregthemusician@gmail.com>
2022-11-01 00:19:26 -0400

    import view-components

--- src/Frame.jsx
+++ src/Frame.jsx
@@ -1,2 +1,6 @@
 import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const Home = lazy(() => import('./view-components/Home'))
+const Events = lazy(() => import('./view-components/Events'))
+const LoadingMessage = () => `loading...`
